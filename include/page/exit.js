e0ff51f9211a4bf92462a9e87673f28c8eb0b3aa
Ruslan Gainutdinov <huksley@sdot.ru>
2016-11-12 00:59:31 +0300

    Properly updating content after logout.

--- include/page/exit.js
+++ include/page/exit.js
@@ -3,8 +3,10 @@ define("page/exit", [ "jquery", "theme", "log", "config", "mustache", "text!temp
     page.init = function () {
         theme.header.off();
         //log.error("Failed to do something!");
+        config.logout();
         $("#content").html(m.render(login, {}));
         config.apply($("#content"));
+        config.apply(document);
     }
     return page;
 });
